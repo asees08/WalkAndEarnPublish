b42dedfde068ae5dfe62a30c635343ab7cb7389a
Asees <nandaasees@gmail.com>
2022-08-15 03:11:37 +0530

    Add project files.

---
+++ Home.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="WalkAndEarn.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        User ID : <asp:Label runat="server" ID="lblID" Text="User Id"></asp:Label>
+        <br />
+        Coins : <asp:Label runat="server" ID="lblCoins" Text="Coins"></asp:Label>
+        <br />
+        Steps : <asp:Label runat="server" ID="lblSteps" Text="Steps"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WalkAndEarn/Home.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WalkAndEarn
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString.Count > 0)
+            {
+                if (!String.IsNullOrEmpty(Request.QueryString["Id"].ToString()))
+                {
+                    lblID.Text = Request.QueryString["Id"].ToString();
+                }
+
+                if (!String.IsNullOrEmpty(Request.QueryString["Coins"].ToString()))
+                {
+                    lblCoins.Text = Request.QueryString["Coins"].ToString();
+                }
+
+                if (!String.IsNullOrEmpty(Request.QueryString["Steps"].ToString()))
+                {
+                    lblSteps.Text = Request.QueryString["Steps"].ToString();
+                }
+
+            }
+        }
+    }
+}
